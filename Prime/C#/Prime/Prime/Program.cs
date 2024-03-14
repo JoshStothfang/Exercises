ec4d17c9803e630d092396ce5500ad37539b492f
Josh Stothfang <josh@joshstothfang.com>
2024-03-14 18:53:03 -0400

    Prime - C#

---
+++ Prime/C#/Prime/Prime/Program.cs
@@ -0,0 +1,21 @@
+﻿int startNum = 2;
+int endNum = 100;
+
+for (int i = startNum; i <= endNum; i++)
+{
+    bool prime = true;
+
+    for (int j = i - 1; j > 1; j--)
+    {
+        if (i % j == 0)
+        {
+            prime = false;
+            break;
+        }
+    }
+
+    if (prime == true)
+    {
+        Console.WriteLine(i);
+    }
+}
