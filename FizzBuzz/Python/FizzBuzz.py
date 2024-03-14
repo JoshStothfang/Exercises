b892c18de038315ed329de9d924ec7e01ec0513f
Josh Stothfang <josh@joshstothfang.com>
2024-03-14 18:13:28 -0400

    FizzBuzz - Python

---
+++ FizzBuzz/Python/FizzBuzz.py
@@ -0,0 +1,12 @@
+startNum = 1
+endNum = 100
+
+for i in range(startNum, endNum + 1):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
