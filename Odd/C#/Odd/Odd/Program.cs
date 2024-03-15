63912cd0c8db4ef322d77f4ce4b708aba0cac3ce
Josh Stothfang <josh@joshstothfang.com>
2024-03-15 09:44:48 -0400

    Odd - C#

---
+++ Odd/C#/Odd/Odd/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+
+int[] numbers = { 25, 1, 52, 85, 93, 44, 17, 36, 12, 94, 4, 27, 89, 99, 49, 22, 12, 36, 58, 43, 29, 91, 4 };
+
+ArrayList oddNumbers = new ArrayList();
+
+foreach (int number in numbers)
+{
+    if (number % 2 != 0)
+    {
+        oddNumbers.Add(number);
+    }
+}
+
+foreach (int oddNumber in oddNumbers)
+{
+    Console.WriteLine(oddNumber);
+}
